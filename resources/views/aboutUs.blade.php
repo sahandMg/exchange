e6ac73cc4399732101d27ce21ce4f3277f8695eb
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-07-30 10:36:17 -0700

    fix nav bar in all pages

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -59,17 +59,8 @@
  .pb_footer {padding-top: 15%;}
 </style>
 
-<script src="assets/js/jquery.min.js"></script>
-
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/slick.min.js"></script>
-<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
-
-<script src="assets/js/jquery.waypoints.min.js"></script>
-<script src="assets/js/jquery.easing.1.3.js"></script>
-
-<script src="assets/js/main.js"></script>
-
 @include('master.scripts')
+<script type="text/javascript">
+    $('#pb-navbar').addClass("scrolled awake"); 
+</script>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -550,5 +550,45 @@
     function isNumeric(num){
        return !isNaN(num)
     }
+
+    // scroll
+    var scrollWindow = function() {
+        $(window).scroll(function(){
+            var $w = $(this),
+                    st = $w.scrollTop(),
+                    navbar = $('.pb_navbar'),
+                    sd = $('.js-scroll-wrap');
+
+            if (st > 150) {
+                if ( !navbar.hasClass('scrolled') ) {
+                    navbar.addClass('scrolled');
+                }
+            }
+            if (st < 150) {
+                if ( navbar.hasClass('scrolled') ) {
+                    navbar.removeClass('scrolled sleep');
+                }
+            }
+            if ( st > 350 ) {
+                if ( !navbar.hasClass('awake') ) {
+                    navbar.addClass('awake');
+                }
+
+                if(sd.length > 0) {
+                    sd.addClass('sleep');
+                }
+            }
+            if ( st < 350 ) {
+                if ( navbar.hasClass('awake') ) {
+                    navbar.removeClass('awake');
+                    navbar.addClass('sleep');
+                }
+                if(sd.length > 0) {
+                    sd.removeClass('sleep');
+                }
+            }
+        });
+    };
+    scrollWindow();
   </script>
 @endsection
