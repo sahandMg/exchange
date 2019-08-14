fc27a86380a38d6db0986e54e0f4804b6fa37610
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-08-14 10:00:44 -0700

    fix exchange pay

--- resources/views/panel/ExchangePaying.blade.php
+++ resources/views/panel/ExchangePaying.blade.php
@@ -3,15 +3,16 @@
     <title> اکسچنج - اسم سایت </title>
 @endsection
 @section('content')
-
-<div class="exchange-card" id="waitForExchange"  style="direction: rtl;">
+<section class="pb_section pb_slant-white" id="section-faq">
+    <div class="container">
+      <div class="exchange-card" id="waitForExchange"  style="direction: rtl;">
            <div class="d-flex justify-content-between">
                <h3>ارسال ارز</h3>
                <span>شناسه انتقال: asdfadsfsadfsadfsd</span>
            </div>
 
            <div class="row">
-               <div class="col-md-7">
+               <div class="col-md-7 col-sm-12">
                    <br/>
                    <p class="text-right">لطفا با استفاده از کیف پول خود، به آدرس زیر پول را ارسال کنید.پس از تایید واریز شما توسط شبکه، به صورت خودکار پول به حساب شما واریز می شود.</p>
                    <div class="exchange-details">
@@ -23,14 +24,19 @@
                        <span class="detail-value">0xa552e9E883636C1aad8FacD3F52DDc2A9d6cbc28</span>
                    </div>
                </div>
-               <div class="col-md-5">
+               <div class="col-md-5  col-sm-12 text-center">
                    <!-- <img src="assets/images/qrcode.png" style="width: 100%;height: auto;"> -->
-                   {!! QrCode::size(150)->generate($trans->payinAddress) !!}
+                   {!! QrCode::size(200)->generate($trans->payinAddress) !!}
                </div>
            </div>
-</div>
+      </div>
+    </div>
+ </section>   
     
 
 @include('master.footer')
-
+@include('master.scripts')
+<script type="text/javascript">
+	$('#pb-navbar').addClass("scrolled awake"); 
+</script>
 @endsection
